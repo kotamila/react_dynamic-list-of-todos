0096f1ba7fed2b3801947fc5c49807632800c29f
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-09-12 18:58:18 +0300

    initial

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,74 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  status: 'all' | 'active' | 'completed';
+  querty: string;
+  onStatusChange: (value: 'all' | 'active' | 'completed') => void;
+  onQuertyChange: (value: string) => void;
+  onClearQuerty: () => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({
+  status,
+  querty,
+  onStatusChange,
+  onQuertyChange,
+  onClearQuerty,
+}) => {
+  return (
+    <form
+      className="field has-addons"
+      onSubmit={event => event.preventDefault()}
+    >
+      <p className="control">
+        <span className="select">
+          <select
+            data-cy="statusSelect"
+            value={status}
+            onChange={event =>
+              onStatusChange(
+                event.target.value as 'all' | 'active' | 'completed',
+              )
+            }
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </span>
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          type="text"
+          className="input"
+          placeholder="Search..."
+          value={querty}
+          onChange={event => onQuertyChange(event.target.value)}
+        />
+        <span className="icon is-left" style={{ pointerEvents: 'all' }}>
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          {querty ? (
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={onClearQuerty}
+            />
+          ) : (
+            <button
+              type="button"
+              className="delete"
+              style={{ visibility: 'hidden' }}
+              aria-hidden
+            ></button>
+          )}
+        </span>
+      </p>
+    </form>
+  );
+};
